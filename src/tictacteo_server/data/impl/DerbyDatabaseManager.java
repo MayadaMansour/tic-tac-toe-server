58c7f624dc11f0cbf0514b1a61d0ab8709113a88
Karim <karim_agha5@hotmail.com>
2023-01-07 17:14:09 +0200

    modified DerbyDatabaseManager

--- src/tictacteo_server/data/impl/DerbyDatabaseManager.java
+++ src/tictacteo_server/data/impl/DerbyDatabaseManager.java
@@ -22,74 +22,43 @@ import java.sql.SQLException;
  * */
 public class DerbyDatabaseManager implements DatabaseManager, Serializable {
 
-    private static volatile DerbyDatabaseManager derbyDatabaseManager;
+    private static DerbyDatabaseManager instance;
     private static Connection connection;
     private final static String DATABASE_URL = "jdbc:derby://localhost:1527/database";
     private final static String USERNAME = "root";
     private final static String PASSWORD = "root";
 
-    private DerbyDatabaseManager(){
-
-        // Prevent instantiation using reflection.
-        if(connection != null){
-            throw new RuntimeException("Can't instantiate using the default constructor. Use getInstance()");
-        }
+    private DerbyDatabaseManager() throws SQLException {
+        DriverManager.registerDriver(new org.apache.derby.jdbc.ClientDriver());
     }
 
-    public static DerbyDatabaseManager getInstance() throws SQLException {
+    public static synchronized DerbyDatabaseManager getInstance() throws SQLException {
 
-        // Double-checked locking.
-        if(derbyDatabaseManager == null){
-
-            synchronized(DerbyDatabaseManager.class){
-
-                if(derbyDatabaseManager == null){
-                    derbyDatabaseManager =  new DerbyDatabaseManager();
-                    DriverManager.registerDriver(new org.apache.derby.jdbc.ClientDriver());
-                }
-            }
+        if(instance == null){
+            instance =  new DerbyDatabaseManager();
         }
-
-        return derbyDatabaseManager;
+        return instance;
     }
+
+
     @Override
     public void start() throws SQLException {
 
-        // If there's an instance of the manager, but it's not connected to the database.
-        if(derbyDatabaseManager != null && connection == null){
+        if(connection == null){
             connection = DriverManager.getConnection(DerbyDatabaseManager.DATABASE_URL,DerbyDatabaseManager.USERNAME
             ,DerbyDatabaseManager.PASSWORD);
         }
     }
 
     @Override
-    public Connection getConnection() throws SQLException {
-
-        // If a connection already exists.
-        if(connection != null){
-            return connection;
-        }
-
-        // If there's an instance of the manager, but it's not connected to the database.
-        // Create a connection and return it.
-        else if(derbyDatabaseManager != null){
-            start();
-            return connection;
-        }
-
-        // There isn't an instance nor a connection. Create both and return a connection.
-        else{
-            getInstance();
-            start();
-            return connection;
-        }
+    public Connection getConnection(){
+        return connection;
     }
 
     @Override
     public void stop() throws SQLException {
-        if(connection != null){
-            connection.close();
-        }
+        connection.close();
+        connection = null;
     }
 
 }
